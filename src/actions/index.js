cdbc9a02802a786c9d59211748539a2fe2f21404
Huiling Loh <huiling_loh_97@hotmail.com>
2022-01-26 23:39:38 +0800

    action, reducers and index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,58 @@
 // TODO: add and export your own actions
+export const FETCH_CARS = "FETCH_CARS"
+export const CAR_CREATED = "CAR_CREATED"
+export const FETCH_CAR = "FETCH_CAR"
+export const CAR_DELETED = "CAR_DELETED"
+
+const base_url = "https://wagon-garage-api.herokuapp.com"
+
+export function fetchCars(garage) {
+  const promise = fetch(`${base_url}/${garage}/cars`)
+    .then(res => res.json())
+
+  return {
+    type: FETCH_CARS,
+    payload: promise
+  }
+}
+
+export function createCar(garage, body, callback) {
+  const promise = fetch(`${base_url}/${garage}/cars`, {
+    method: "POST",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  })
+  .then(res => res.json())
+  .then(callback)
+
+  return {
+    type: CAR_CREATED,
+    payload: promise
+  }
+}
+
+export function fetchCar(id) {
+  const promise = fetch(`${base_url}/cars/${id}`)
+    .then(res => res.json())
+
+    return {
+      type: FETCH_CAR,
+      payload: promise
+    }
+}
+
+export function deleteCar(id, callback) {
+  const promise = fetch(`${base_url}/cars/${id}`, {
+    method: "DELETE"
+  })
+  .then(res => res.json())
+  .then(callback)
+
+  return {
+    type: CAR_DELETED,
+    payload: promise
+  }
+}
--- src/index.jsx
+++ src/index.jsx
@@ -9,19 +9,38 @@ import { createHistory as history } from 'history';
 
 import '../assets/stylesheets/application.scss';
 
+import carsReducer from './reducers/cars_reducer';
+import { reducer as formReducer } from 'redux-form';
+
+import carsIndex from './containers/cars_index';
+import carsNew from './containers/cars_new';
+import carShow from './containers/car_show';
+
+const initialState = {
+  garage: "my-awesome-garage", //prompt('Please key in your garage name') ||
+  cars: []
+}
+
 const reducers = combineReducers({
-  // key: reducer
+  cars: carsReducer,
+  form: formReducer
 });
 
 const middlewares = applyMiddleware(reduxPromise, logger);
+const store = createStore(reducers, initialState, middlewares)
 
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers, {}, middlewares)}>
+  <Provider store={store}>
     <Router history={history}>
-      <Switch>
-        TODO
-      </Switch>
+      <div className="view-container">
+        <Switch>
+          <Route path="/" exact component={carsIndex}/>
+          <Route path="/car/new" exact component={carsNew} />
+          <Route path="/car/:id" component={carShow} />
+          <Route path="/car/:id/delete"/>
+        </Switch>
+      </div>
     </Router>
   </Provider>,
   document.getElementById('root')
